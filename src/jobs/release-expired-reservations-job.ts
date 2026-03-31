bcaec2b18ae4790e7eee31e0fad011d5e4bada51
Viviane Aguiar <vivianeaguiarc@outlook.com>
2026-03-30 21:21:36 -0300

    feat: add background job to release expired reservations automatically

---
+++ src/jobs/release-expired-reservations-job.ts
@@ -0,0 +1,11 @@
+import { ReleaseExpiredReservationsUseCase } from '../use-cases/release-expired-reservations-use-case.js'
+
+export const startReleaseExpiredReservationsJob = (): void => {
+  setInterval(async () => {
+    try {
+      await ReleaseExpiredReservationsUseCase.execute()
+    } catch (error) {
+      console.error('Error releasing expired reservations:', error)
+    }
+  }, 60 * 1000)
+}
